a016b8549fde1f10a25555a2673b4109b0f465e1
alvarezdmarch <159318446+alvarezdmarch@users.noreply.github.com>
2025-02-22 10:33:31 -0500

    deploy fix attempt

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: "/portfolio2",
+    assetPrefix: "/portfolio2/",
+  };
+  
+  module.exports = nextConfig;
+  
